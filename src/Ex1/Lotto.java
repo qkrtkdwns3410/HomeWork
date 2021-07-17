211c067182828795adb932933debe8684578e58a
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-07-17 16:36:56 +0900

    7-14

---
+++ src/Ex1/Lotto.java
@@ -0,0 +1,84 @@
+package Ex1;
+
+public class Lotto {
+    static int[] lo = new int[6];
+    public int num = (int) (Math.random() * 45 + 1);
+
+    public Lotto() {
+    }
+
+
+    public  void bubbleSort() {
+
+        for (int i = 0; i < lo.length - 1; i++) {
+
+            for (int j = 0; j < lo.length - i - 1; j++) {
+
+                if (lo[j] > lo[j + 1]) {
+
+                    int temp = lo[j];
+                    lo[j] = lo[j + 1];
+                    lo[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+
+    public void lottoDiff() {
+
+        for (int i = 0; i < lo.length; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                while (lo[i] == lo[j]) {
+                    lo[i] = (int) (Math.random() * 45 + 1); //값이 같으면 다시 랜덤값을 넣어줍니다.
+                }
+
+            }
+
+        }
+    }
+
+    public void lottoPick() {
+
+        for (int i = 0; i < lo.length; i++) {
+
+            lo[i] = num;
+        }
+
+    }
+
+    public void print(int[] lotto) {
+        for (int i : lotto) {
+            System.out.println(i);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Ex1/Main.java
+++ src/Ex1/Main.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-//1. StringBuffer 활용하는 방법
-//        StringBuffer sb = new StringBuffer();
-//        sb.append(str);
-//        System.out.println(sb.reverse());
+        int count = sc.nextInt(); //count 값을 받습니다. =5
+        int[] arr = new int[count];
 
-        //2. char 배열을 활용하는 방법
-        char[] chArr = new char[str.length()];
-
-        for (int i = 0; i < chArr.length; i++) {
-            chArr[i] = str.charAt(i); //문자열을 한글자씩 담습니다.
-        }
-        for (int i = chArr.length - 1; i >= 0; i--) {
-            System.out.printf("%s", chArr[i]);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
 
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
     }
 
 }
